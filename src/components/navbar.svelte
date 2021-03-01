107682deb7b37fea62b82df0cf1b76d44e1f951d
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-01 14:07:53 +0100

    added component

---
+++ src/components/navbar.svelte
@@ -0,0 +1,12 @@
+<p>This is another paragraph.</p>
+<div id="horisontalStripes" class="pattern"/>
+<div id="verticalStripes" class="pattern"/>
+
+<style>
+    #horisontalStripes {
+        background-image: repeating-linear-gradient(90deg,rgb(100, 0, 0, 0.5),rgb(150, 0, 0, 0.5) 5vw, rgb(256, 256, 256, 0.5) 5vw, rgb(256, 256, 256, 0.5) 10vw);
+    }
+    #verticalStripes {
+        background-image: repeating-linear-gradient(180deg, rgb(150, 0, 0, 0.5),rgb(150, 0, 0, 0.5) 5vw, rgb(256, 256, 256, 0.5) 5vw, rgb(256, 256, 256, 0.5) 10vw);
+    }
+</style>
