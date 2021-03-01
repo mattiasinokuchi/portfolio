2e1d03d203f898d7f99cb35fb265d9017311a1d4
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-01 14:16:32 +0100

    added css

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,4 +1,18 @@
+<style>
+    nav {
+        position: fixed;
+        z-index: 1;
+        background: rgb(200, 150, 150, 1.0);
+        width: 100%;
+        height: 5vw;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+</style>
+
 <nav id="navbar">
     <a href="#projects">Projects</a>
     <a href="#links">Links</a>
 </nav>
+
