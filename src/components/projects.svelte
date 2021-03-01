d47f40b6622248383feac31363c4e59678c81105
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-01 16:31:45 +0100

    added projects

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,3 +1,19 @@
+<script>
+	let projects = [
+		{ id: 'J---aiyznGQ', name: '😀 Multiplication driller' },
+		{ id: 'z_AbfPXTKms', name: '😀 Japanese speaking memory game' },
+		{ id: 'OUtn3pvWmpg', name: '🙂 Seeding machine calculator' }
+	];
+</script>
+
 <header>
     <h2>Web projects from past to present</h2>
 </header>
+
+<ul>
+	{#each projects as { id, name }}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}">
+		    {name}
+		</a></li>
+	{/each}
+</ul>
