43964b90d82cdb4bc57371a0cad5dacc3d2e9214
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-01 15:20:53 +0100

    added background

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -5,5 +5,13 @@ import Navbar from './components/navbar.svelte';
 import Header from './components/header.svelte';
 </script>
 
+<style>
+	:global(body){
+    	background-image:
+			repeating-linear-gradient(90deg,rgb(150, 0, 0, 0.5),rgb(150, 0, 0, 0.5) 5vw, rgb(256, 256, 256, 0.5) 5vw, rgb(256, 256, 256, 0.5) 10vw),
+			repeating-linear-gradient(180deg, rgb(150, 0, 0, 0.5),rgb(150, 0, 0, 0.5) 5vw, rgb(256, 256, 256, 0.5) 5vw, rgb(256, 256, 256, 0.5) 10vw);
+    }
+</style>
+
 <Navbar/>
 <Header/>
