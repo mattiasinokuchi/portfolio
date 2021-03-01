9db356a0df5ff473e02a2665fef6fcb672551f8d
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-01 16:55:27 +0100

    added href, alt and src

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,8 +1,23 @@
 <script>
 	let projects = [
-		{ name: '😀 Multiplication driller', href: 'https://mattiasinokuchi.github.io/multiplication/' },
-		{ name: '😀 Japanese speaking memory game', href: 'https://mattiasinokuchi.github.io/memory/' },
-		{ name: '🙂 Seeding machine calculator' , href: 'https://mattiasinokuchi.github.io/seedingcalculator/' }
+		{
+            name: '😀 Multiplication driller',
+            href: 'https://mattiasinokuchi.github.io/multiplication/',
+            alt: 'screenshot showing 8x9=72',
+            src: 'https://raw.githubusercontent.com/mattiasinokuchi/multiplication/master/screenshot.png'
+        },
+		{
+            name: '😀 Japanese speaking memory game',
+            href: 'https://mattiasinokuchi.github.io/memory/',
+            alt: 'screenshot showing memory game with animals',
+            src: 'https://raw.githubusercontent.com/mattiasinokuchi/memory/master/screenshot.png'
+        },
+		{
+            name: '🙂 Seeding machine calculator',
+            href: 'https://mattiasinokuchi.github.io/seedingcalculator/',
+            alt: 'screenshot showing a calculator',
+            src: 'https://raw.githubusercontent.com/mattiasinokuchi/seedingcalculator/master/screenshot.png'
+        }
 	];
 </script>
 
@@ -11,9 +26,10 @@
 </header>
 
 <ul>
-	{#each projects as { name, href }}
+	{#each projects as { name, href, alt, src }}
 		<li>
             <a target="_blank" href={href}>
+                <img alt={alt} src={src}>
 		        {name}
 		    </a>
         </li>
