477c08755a9ce626a521318d3beefe18f29afd55
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-02 17:28:16 +0100

    added contact form

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -5,9 +5,11 @@
 	import Header from './components/header.svelte';
 	import Projects from './components/projects.svelte';
 	import Links from './components/links.svelte';
+	import Contact from './components/form.svelte';
 </script>
 
 <Navbar/>
 <Header/>
 <Projects/>
 <Links/>
+<Contact/>
---
+++ src/components/form.svelte
@@ -0,0 +1,18 @@
+<h3>Get in touch with me!</h3>
+<form name="contact" method="POST" data-netlify="true">
+    <p>
+        <label for="Name">Name</label>
+        <input type="text" id="name" name="name" />
+    </p>
+    <p>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+    </p>
+    <p>
+        <label for="message">Message</label>
+        <textarea id="message" name="message"></textarea>
+    </p>
+    <p>
+        <button type="submit">Send</button>
+    </p>
+</form>
