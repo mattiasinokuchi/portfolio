8dcbc7898dbbc5b7d395c01cb9742d8dcfea3a6a
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-02 17:17:10 +0100

    added links

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -3,9 +3,11 @@
 <script>
 	import Navbar from './components/navbar.svelte';
 	import Header from './components/header.svelte';
-	import Projects from './components/projects.svelte'
+	import Projects from './components/projects.svelte';
+	import Links from './components/links.svelte';
 </script>
 
 <Navbar/>
 <Header/>
 <Projects/>
+<Links/>
---
+++ src/components/links.svelte
@@ -0,0 +1,7 @@
+<h2>Links</h2>
+<p>
+  <a id="profile-link" href="https://www.freecodecamp.org/fcc69cdeb32-da30-4e3b-a1bf-d3e332a2de68" target="_blank" rel="noopener noreferrer">My profile at freeCodeCamp</a>
+</p>
+<p>
+    <a id="profile-link" href="https://github.com/mattiasinokuchi" target="_blank" rel="noopener noreferrer">My profile at GitHub</a>    
+</p>
