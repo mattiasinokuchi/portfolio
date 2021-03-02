ddd4590b17a872573aed721013940cf7319d7487
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-02 14:44:37 +0100

    added styling

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -21,6 +21,38 @@
 	];
 </script>
 
+<style>
+
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        list-style-type: none;
+    }
+
+    li {
+        margin-bottom: 10vw;
+        border-radius: 40vw;
+        box-shadow: inset -0.1vw -0.1vw 0.2vw 0.2vw grey, inset 0 0 0.5vw 1vw white, inset 2vw 2vw 0.5vw 0.5vw grey, inset 0 0 2vw 2vw grey, 1vw 1vw 1vw 1vw rgb(0, 0, 0, 0.7);
+        width: 40vw;
+        height: 40vw;
+        background: white;
+        top: 50%;
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 3vw;
+        color: black;
+    }
+
+    img {
+        width: 25vw;
+        margin-top: 11vw;
+    }
+
+</style>
+
 <header>
     <h2>Web projects from past to present</h2>
 </header>
@@ -30,8 +62,8 @@
 		<li>
             <a target="_blank" href={href}>
                 <img alt={alt} src={src}>
-		        {name}
 		    </a>
+            <h3>{name}</h3>
         </li>
 	{/each}
 </ul>
