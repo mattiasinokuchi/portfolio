5de3e5ed70996a53e5a0bdcaef3a173aa14f0c9b
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-03 13:37:01 +0100

    fixed link

--- src/components/links.svelte
+++ src/components/links.svelte
@@ -1,4 +1,4 @@
-<h2>Links</h2>
+<h2 id="links">Links</h2>
 <p>
   <a id="profile-link" href="https://www.freecodecamp.org/fcc69cdeb32-da30-4e3b-a1bf-d3e332a2de68" target="_blank" rel="noopener noreferrer">My profile at freeCodeCamp</a>
 </p>
