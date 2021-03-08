a2f16cc740e3a349c075f3f7ad45d7e0a801bd9e
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-03-08 06:29:01 +0100

    removed faulty transition

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,11 +1,9 @@
 <script>
 
-    import { fade } from 'svelte/transition';
-
 </script>
 <style>
 
 </style>
 
-<h1 transition:fade>Welcome to my portfolio</h1>
+<h1>Welcome to my portfolio</h1>
 <h3>Scroll down</h3>
