85975e6d2087e0d0d51e45d0fdfa69a7877038da
mattiasinokuchi <8-b@tuta.io>
2021-04-19 13:54:28 +0200

    removed Content-Type

--- src/components/form.svelte
+++ src/components/form.svelte
@@ -1,9 +1,31 @@
+<h2>Get in touch with me!</h2>
+<form name="contact" method="POST" data-netlify="true">
+    <p>
+        <label for="Name">Name</label>
+        <input type="text" id="name" name="name" />
+    </p>
+    <p>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+    </p>
+    <p>
+        <label for="message">Message</label>
+        <textarea id="message" name="message" />
+    </p>
+    <p>
+        <button type="submit">Send</button>
+    </p>
+</form>
+
 <style>
-    input, textarea, button {
+    input,
+    textarea,
+    button {
         font-size: 3vw;
     }
 
-    input, textarea {
+    input,
+    textarea {
         width: 40vw;
         height: 10vh;
     }
@@ -17,12 +39,14 @@
     }
 
     @media (max-width: 800px) {
-        
-        input, textarea, button {
+        input,
+        textarea,
+        button {
             font-size: 6vw;
         }
 
-        input, textarea {
+        input,
+        textarea {
             width: 80vw;
             height: 10vh;
         }
@@ -30,31 +54,5 @@
         textarea {
             height: 20vh;
         }
-
     }
-
 </style>
-
-<h2>Get in touch with me!</h2>
-<form
-    name="contact"
-    method="POST"
-    data-netlify="true"
-    Content-Type="application/x-www-form-urlencoded"
-    >
-    <p>
-        <label for="Name">Name</label>
-        <input type="text" id="name" name="name" />
-    </p>
-    <p>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" />
-    </p>
-    <p>
-        <label for="message">Message</label>
-        <textarea id="message" name="message"></textarea>
-    </p>
-    <p>
-        <button type="submit">Send</button>
-    </p>
-</form>
