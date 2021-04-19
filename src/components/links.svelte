be75bc619c3aa6aebdddf6fd9b23ea18022e4658
mattiasinokuchi <8-b@tuta.io>
2021-04-19 13:32:00 +0200

    added link and removed import

--- src/components/links.svelte
+++ src/components/links.svelte
@@ -1,7 +1,25 @@
 <h2 id="links">Links</h2>
 <p>
-  <a id="profile-link" href="https://www.freecodecamp.org/fcc69cdeb32-da30-4e3b-a1bf-d3e332a2de68" target="_blank" rel="noopener noreferrer">My profile at freeCodeCamp</a>
+  <a
+    id="profile-link"
+    href="https://www.freecodecamp.org/fcc69cdeb32-da30-4e3b-a1bf-d3e332a2de68"
+    target="_blank"
+    rel="noopener noreferrer">My profile at freeCodeCamp</a
+  >
 </p>
 <p>
-    <a id="profile-link" href="https://github.com/mattiasinokuchi" target="_blank" rel="noopener noreferrer">My profile at GitHub</a>    
+  <a
+    id="profile-link"
+    href="https://github.com/mattiasinokuchi"
+    target="_blank"
+    rel="noopener noreferrer">My profile at GitHub</a
+  >
+</p>
+<p>
+  <a
+    id="profile-link"
+    href="https://www.humanetech.com/"
+    target="_blank"
+    rel="noopener noreferrer">My least but most important link - Center for Humane Technology</a
+  >
 </p>
