3ed007ca01d22df6d351151098d998b78073c760
mattiasinokuchi <8-b@tuta.io>
2021-04-28 11:18:32 +0200

    moved styling of h2 and added top margin

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,10 +1,28 @@
 <script>
-
-    import { projects } from './stores.js';
-
+    import { projects } from "./stores.js";
 </script>
 
+<h2 id="projects">Web projects from past to present</h2>
+
+<ul>
+    {#each $projects as project}
+        <li>
+            <a target="_blank" href={project.href}>
+                <img alt={project.alt} src={project.src} />
+            </a>
+        </li>
+        <div>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+        </div>
+    {/each}
+</ul>
+
 <style>
+    h2 {
+        font-size: 4vw;
+        padding-top: 40vh;
+    }
 
     ul {
         display: flex;
@@ -16,7 +34,9 @@
     li {
         margin-bottom: 10vw;
         border-radius: 40vw;
-        box-shadow: inset -0.1vw -0.1vw 0.2vw 0.2vw grey, inset 0 0 0.5vw 1vw white, inset 2vw 2vw 0.5vw 0.5vw grey, inset 0 0 2vw 2vw grey, 1vw 1vw 1vw 1vw rgb(0, 0, 0, 0.7);
+        box-shadow: inset -0.1vw -0.1vw 0.2vw 0.2vw grey,
+            inset 0 0 0.5vw 1vw white, inset 2vw 2vw 0.5vw 0.5vw grey,
+            inset 0 0 2vw 2vw grey, 1vw 1vw 1vw 1vw rgb(0, 0, 0, 0.7);
         width: 40vw;
         height: 40vw;
         background: white;
@@ -40,14 +60,20 @@
     }
 
     @media (max-width: 800px) {
+        h2 {
+            font-size: 8vw;
+            padding-top: 20vw;
+            margin-top: 20vh;
+        }
 
         li {
             width: 80vw;
             height: 80vw;
             margin-bottom: 1vw;
             border-radius: 80vw;
-            box-shadow: inset -0.2vw -0.2vw 0.4vw 0.4vw grey, inset 0 0 1vw 2vw white, inset 4vw 4vw 1vw 1vw grey, inset 0 0 4vw 4vw grey, 2vw 2vw 2vw 2vw rgb(0, 0, 0, 0.7);
-
+            box-shadow: inset -0.2vw -0.2vw 0.4vw 0.4vw grey,
+                inset 0 0 1vw 2vw white, inset 4vw 4vw 1vw 1vw grey,
+                inset 0 0 4vw 4vw grey, 2vw 2vw 2vw 2vw rgb(0, 0, 0, 0.7);
         }
 
         div {
@@ -59,23 +85,5 @@
             width: 50vw;
             margin-top: 22vw;
         }
-
     }
-
 </style>
-
-<h2 id="projects">Web projects from past to present</h2>
-
-<ul>
-    {#each $projects as project}
-        <li>
-            <a target="_blank" href={project.href}>
-                <img alt={project.alt} src={project.src}>
-		    </a>
-        </li>
-        <div>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>    
-        </div>
-    {/each} 
-</ul>
