910a1b6800b17892743e31fe10e3e18dd1174c31
mattiasinokuchi <8-b@tuta.io>
2021-04-28 11:05:30 +0200

    added transistion

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,20 +1,57 @@
 <script>
+    import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
+
+    let showText;
+
+    onMount(async () => {
+        await delay(1000);
+        showText = "Hi! I'm Mattias.";
+        await delay(2000);
+        showText = false;
+        await delay(2000);
+        showText = "Welcome to my portfolio!";
+    });
+
+    function delay(delayInms) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(2);
+            }, delayInms);
+        });
+    }
 </script>
 
-<img
-    src="https://raw.githubusercontent.com/mattiasinokuchi/portfolio/main/src/assets/me.jpeg"
-    alt="me eating icecream"
-/>
-<h1>Hi! I'm Mattias. Welcome to my portfolio!</h1>
-<h4>Scroll down</h4>
+<header>
+    <img
+        src="https://raw.githubusercontent.com/mattiasinokuchi/portfolio/main/src/assets/me.jpeg"
+        alt="me eating icecream"
+    />
+    {#if showText}
+    <h1 transition:fade>{showText}</h1>
+  {/if}
+</header>
 
 <style>
+    header {
+        display: flex;
+    }
     h1 {
         font-size: 5vw;
     }
     img {
+        width: 50vw;
+        margin-top: 20vw;
+        background: white;
+        padding: 2vw;
+        border: 1px solid grey;
+        box-shadow: 0.5vw 0.5vw 1vw black;
+        transform: rotate(5deg);
     }
     @media (max-width: 800px) {
+        header {
+            display: block;
+        }
         h1 {
             font-size: 10vw;
         }
