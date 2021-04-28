44ba0114c6e0e31ead0be78d008f5c306e425e64
mattiasinokuchi <8-b@tuta.io>
2021-04-28 10:36:32 +0200

    added photo

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,9 +1,31 @@
 <script>
-
 </script>
+
+<img
+    src="https://raw.githubusercontent.com/mattiasinokuchi/portfolio/main/src/assets/me.jpeg"
+    alt="me eating icecream"
+/>
+<h1>Hi! I'm Mattias. Welcome to my portfolio!</h1>
+<h4>Scroll down</h4>
+
 <style>
-
+    h1 {
+        font-size: 5vw;
+    }
+    img {
+    }
+    @media (max-width: 800px) {
+        h1 {
+            font-size: 10vw;
+        }
+        img {
+            width: 50vw;
+            margin-top: 20vw;
+            background: white;
+            padding: 2vw;
+            border: 1px solid grey;
+            box-shadow: 0.5vw 0.5vw 1vw black;
+            transform: rotate(5deg);
+        }
+    }
 </style>
-
-<h1>Welcome to my portfolio</h1>
-<h3>Scroll down</h3>
