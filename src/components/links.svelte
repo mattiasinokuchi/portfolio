56423e81029c83b8ecebca528e8b2eb55bbaa299
mattiasinokuchi <8-b@tuta.io>
2021-04-29 09:24:14 +0200

    set height

--- src/components/links.svelte
+++ src/components/links.svelte
@@ -1,37 +1,44 @@
-<h2 id="links">Links</h2>
-<p>
-  <a
-    id="profile-link"
-    href="https://www.freecodecamp.org/fcc69cdeb32-da30-4e3b-a1bf-d3e332a2de68"
-    target="_blank"
-    rel="noopener noreferrer">My certificates</a
-  >
-</p>
-<p>
-  <a
-    id="profile-link"
-    href="https://github.com/mattiasinokuchi"
-    target="_blank"
-    rel="noopener noreferrer">My place at GitHub</a
-  >
-</p>
-<p>
-  <a
-    id="profile-link"
-    href="https://www.humanetech.com/"
-    target="_blank"
-    rel="noopener noreferrer">Social media?</a
-  >
-</p>
+<section>
+  <h2 id="links">Links</h2>
+  <p>
+    <a
+      id="profile-link"
+      href="https://www.freecodecamp.org/fcc69cdeb32-da30-4e3b-a1bf-d3e332a2de68"
+      target="_blank"
+      rel="noopener noreferrer">My certificates</a
+    >
+  </p>
+  <p>
+    <a
+      id="profile-link"
+      href="https://github.com/mattiasinokuchi"
+      target="_blank"
+      rel="noopener noreferrer">My place at GitHub</a
+    >
+  </p>
+  <p>
+    <a
+      id="profile-link"
+      href="https://www.humanetech.com/"
+      target="_blank"
+      rel="noopener noreferrer">Social media?</a
+    >
+  </p>
+</section>
 
 <style>
+  section {
+    height: 100vh;
+  }
+
   h2 {
     font-size: 4vw;
     text-align: center;
     padding-top: 20vh;
   }
 
-  p, a {
+  p,
+  a {
     font-size: 3vw;
     text-align: center;
   }
@@ -41,7 +48,8 @@
       font-size: 7vw;
     }
 
-    p, a {
+    p,
+    a {
       font-size: 5vw;
     }
   }
