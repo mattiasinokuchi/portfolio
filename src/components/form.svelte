a36e5a60a302ad438bd2178e561dde4e53afe872
mattiasinokuchi <8-b@tuta.io>
2021-04-29 09:26:03 +0200

    set height

--- src/components/form.svelte
+++ src/components/form.svelte
@@ -1,25 +1,31 @@
-<h2 id="contact">Get in touch with me!</h2>
-<form name="contact" method="POST" data-netlify="true">
-    <!--Hidden input field needed for form handling in Netlify-->
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-        <label for="Name">Name</label>
-        <input type="text" id="name" name="name" />
-    </p>
-    <p>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" />
-    </p>
-    <p>
-        <label for="message">Message</label>
-        <textarea id="message" name="message" />
-    </p>
-    <p>
-        <button type="submit">Send</button>
-    </p>
-</form>
+<section>
+    <h2 id="contact">Get in touch with me!</h2>
+    <form name="contact" method="POST" data-netlify="true">
+        <!--Hidden input field needed for form handling in Netlify-->
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+            <label for="Name">Name</label>
+            <input type="text" id="name" name="name" />
+        </p>
+        <p>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" />
+        </p>
+        <p>
+            <label for="message">Message</label>
+            <textarea id="message" name="message" />
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
+</section>
 
 <style>
+    section {
+        height: 100vh;
+    }
+
     h2 {
         font-size: 4vw;
         text-align: center;
