c1f92b69853c2591d1d2d28ecb6769dbcebfbaa6
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-07-27 05:34:23 +0200

    changed description

--- src/components/stores.js
+++ src/components/stores.js
@@ -2,57 +2,57 @@ import { readable } from 'svelte/store';
 
 export let projects = readable([
     {
-        name: '😀 Multiplication driller',
-        description: 'My first web app. Written in a text editor. So simple! So fun!',
+        name: 'Multiplication driller',
+        description: 'My first web app. Written in a text editor. Simple and fun!',
         href: 'https://mattiasinokuchi.github.io/multiplication/',
         alt: 'screenshot showing eight times nine is equal to seventy-two',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/multiplication/master/screenshot.png'
     },
     {
-        name: '😀 Japanese speaking memory game',
-        description: 'Next project was clearly over my level but I had fun and learned by poking around with code from more clever people.',
+        name: 'Memory game with Japanese speech synthesis',
+        description: 'I hade little idea what I was doing but lots of fun poking around with code from others.',
         href: 'https://mattiasinokuchi.github.io/memory/',
         alt: 'screenshot showing memory game with animals',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/memory/master/screenshot.png'
     },
     {
-        name: '🙂 Seeding machine calculator',
-        description: 'My first idea was to build web applications for people near me. My brother-in-law might be the only one using this one.',
+        name: 'Seeding machine calculator',
+        description: 'An app for my brother-in-law saving him from headache when farming vegetables.',
         href: 'https://mattiasinokuchi.github.io/seedingcalculator/',
         alt: 'screenshot showing a calculator',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/seedingcalculator/master/screenshot.png'
     },
     {
-        name: '🙂 The little census',
-        description: 'My Sister needed a visitor counter due to the Pandemia. Authentication, localization and server rendering. Similar to my last project but a lot slower.',
+        name: 'Online visitor counter',
+        description: 'My Sister needed to count visitors due to the Pandemia. I took the chance to practice authentication, localization and server rendering. Serverless would have made it a lot faster.',
         href: 'http://little-census.se/',
         alt: 'screenshot of an app which counts people',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/little-census/main/screenshot.png'
     },
     {
-        name: '🙂 American / British English Translator',
-        description: 'A freeCodeCamp project. I learned (and forgot) regular expressions like "(?&lt!\\w|-)". Looks like swearing to me',
+        name: 'American / British English Translator',
+        description: 'A freeCodeCamp project. I learned (and forgot) regular expressions. "(?&lt!\\w|-)" still looks a lot like swearing to me',
         href: 'https://american-british-english-translator.mattiasinokuchi.repl.co/',
         alt: 'screenshot of an app which translates between american and brittish english',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/american-british-english-translator/master/screenshot.png'
     },
     {
-        name: '😀 URL Shortener',
-        description: 'A modern version of my first back-end challenge from freeCodeCamp using Svelte, serverless functions and MongoDB.',
+        name: 'Make long links shorter',
+        description: 'My first back-end challenge at freeCodeCamp was as exciting as my first app. Something out there responded to me! Later on I discovered how to make my backend projects working fast at no cost using serverless functions.',
         href: 'https://re-url.vercel.app',
         alt: 'screenshot of an app which shortens a URL',
         src: 'https://github.com/mattiasinokuchi/serverless-url-shortener/blob/master/screenshot.png?raw=true'
     },
     {
-        name: '😂 Sudoku solver',
-        description: 'Remake of a freeCodeCamp project. I still get tears in my eyes when I talk about the moment it worked.',
+        name: 'Sudoku solver',
+        description: 'I still get tears in my eyes when I talk about the moment this freeCodeCamp project started working. I thought it deserved a remake with better front. Using Svelte was my only option here.',
         href: 'https://su-do-ku.netlify.app',
         alt: 'screenshot of an app which solves sudoku',
         src: 'https://github.com/mattiasinokuchi/svelte-sudoku-solver/raw/master/Screenshot.png'
     },
     {
-        name: '🙂 List personal things!',
-        description: 'Authentication with Magic Link. So much easier to understand than any other I have tried. Besides, who likes passwords?',
+        name: 'Your online private list',
+        description: 'Magic Link is the easiest (safe) way of authenticate users I have tried. And the best things is...no passwords to forget, change or steal!!!',
         href: 'https://magic-svelte.netlify.app',
         alt: 'screenshot of an app which lists personal things',
         src: 'https://github.com/mattiasinokuchi/magic-svelte/raw/master/Screenshot.png'
