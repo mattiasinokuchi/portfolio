2edaa036c2079f02745e8b9d825d19734e5a5428
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-07-28 06:39:12 +0200

    changed text content

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -1,12 +1,12 @@
 <section>
     <h2 id="about">About</h2>
-    <p>This is a story about a bored Swedish chemist called Mattias. The adventure starts one night when his thoughts flies out the window to a search for his lost passion.
+    <p>This is a story about Mattias - a bored Swedish chemist. The adventure starts one night when his thoughts flies out the window to a search for his lost passion.
     </p>
     <p>With the understanding from his work, colleagues, wife and two children he goes to the land of coding.
     </p>
-    <p>A land in the coffee room, bus stop, and mostly...a poorly ventilated scrub. A thousand early mornings, lunch breaks and afternoons full of HTML, CSS, Javascript and Mattias worst enemy - Captain Time and villainous Dev Tools.
+    <p>A land in the coffee room, bus stop, and mostly...a poorly ventilated scrub. Early mornings, lunch breaks and afternoons full of adventures with his friends HTML, CSS, Javascript and Mattias worst enemy - Captain Time and the villainous Dev Tools.
     </p>
-    <p>Mattias is the story about the boy who never wants to grow up and dreams that can come true.
+    <p>Mattias is the story about the boy who never wants to grow up and dreams that come true.
     </p>
   </section>
   
