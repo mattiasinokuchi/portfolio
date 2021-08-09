280aa857600f429b5febc6395e88cf2e68201317
mattiasinokuchi <8-b@tuta.io>
2021-08-09 11:31:00 +0200

    changed text content

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -1,12 +1,10 @@
 <section>
     <h2 id="about">About</h2>
-    <p>This is a story about Mattias - a bored Swedish chemist. The adventure starts one night when his thoughts flies out the window to a search for his lost passion.
+    <p>One night the thoughts of a bored Swedish chemist flies out the window to a search for his lost passion.
     </p>
     <p>With the understanding from his work, colleagues, wife and two children he goes to the land of coding.
     </p>
-    <p>A land in the coffee room, bus stop, and mostly...a poorly ventilated scrub. Early mornings, lunch breaks and afternoons full of adventures with his friends HTML, CSS, Javascript and the worst enemies - Captain Time and the Development Environment Setup Pirates.
-    </p>
-    <p>Mattias is the story about the boy who wants to make good dreams come true and never want to grow up.
+    <p>A land in the coffee room, bus stop, and mostly...a poorly ventilated scrub. Early mornings, introvert lunch breaks and hidden afternoons of devotion, despair and dopamine kicks.
     </p>
   </section>
   
