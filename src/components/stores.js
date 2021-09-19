e522ef54d9ec2d1a4230b4393aaab9fb0cbde837
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-09-19 20:02:25 +0200

    Update stores.js

--- src/components/stores.js
+++ src/components/stores.js
@@ -3,21 +3,21 @@ import { readable } from 'svelte/store';
 export let projects = readable([
     {
         name: 'Multiplication driller',
-        description: 'My first web app written in a text editor. Rediscovering coding was very fun! My app could be seen and used by anybody! I quickly discovered styling with CSS to be the most time consuming part and that amazing things could happen by mistake.',
+        description: 'My first web app written in a text editor. Rediscovering coding was fun! My app could be seen and used by anybody! I quickly discovered styling and CSS was most time consuming and that amazing things could happen by mistake.',
         href: 'https://mattiasinokuchi.github.io/multiplication/',
         alt: 'screenshot showing eight times nine is equal to seventy-two',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/multiplication/master/screenshot.png'
     },
     {
         name: 'Seeding machine calculator',
-        description: 'An app for my brother-in-law saving him from headache when farming vegetables. Keeping the DOM (a model and bridge to the browser) in sync with the state of my app was tricky using vanilla Javascript.',
+        description: 'An app for my brother-in-law saving him from headache when farming vegetables. Keeping the DOM (a model for the browser) in sync with the state of my app was a pain (using plain Javascript).',
         href: 'https://mattiasinokuchi.github.io/seedingcalculator/',
         alt: 'screenshot showing a calculator',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/seedingcalculator/master/screenshot.png'
     },
     {
         name: 'Online visitor counter',
-        description: 'My Sister needed to count visitors due to the Pandemia. I took the chance to practice authentication, localization and server rendering. Also an example of that servers are slow unless you pay for them.',
+        description: 'My Sister needed to count visitors due to the Pandemia. I took the chance to practice authentication, localization and server rendering. An example of how slow servers are (unless you pay for them).',
         href: 'http://little-census.se/',
         alt: 'screenshot of an app which counts people',
         src: 'https://raw.githubusercontent.com/mattiasinokuchi/little-census/main/screenshot.png'
@@ -31,14 +31,14 @@ export let projects = readable([
     },
     {
         name: 'Make long links shorter',
-        description: 'My first back-end challenge at freeCodeCamp was as exciting as my first app. Something out there responded to me! Later on I discovered how to make my backend projects working a lot faster at no cost using serverless functions.',
+        description: 'My first back-end challenge at freeCodeCamp was as exciting as my first app. Something out there responded to me! Later on I discovered how to make my backend projects work a lot faster at no cost using serverless functions.',
         href: 'https://re-url.vercel.app',
         alt: 'screenshot of an app which shortens a URL',
         src: 'https://github.com/mattiasinokuchi/serverless-url-shortener/blob/master/screenshot.png?raw=true'
     },
     {
         name: 'Sudoku solver',
-        description: 'I still get tears in my eyes when I talk about the moment this freeCodeCamp project started working. I thought it deserved a remake with better front. Using Svelte was my only option here.',
+        description: 'I still get tears in my eyes when I talk about the moment this freeCodeCamp project started to work. I thought it deserved a remake with better front and Svelte was my only option here.',
         href: 'https://su-do-ku.netlify.app',
         alt: 'screenshot of an app which solves sudoku',
         src: 'https://github.com/mattiasinokuchi/svelte-sudoku-solver/raw/master/Screenshot.png'
